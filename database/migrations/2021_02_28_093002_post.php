ad64dffcbb529caed7a47493bdaf6fe3ff0e5ac7
Kuanysh <75529452+panshenkowski@users.noreply.github.com>
2021-02-28 14:36:29 +0500

    create post table

---
+++ database/migrations/2021_02_28_093002_post.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Post extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->text('body');
+            $table->timestamp('timestamps')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
